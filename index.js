547504fbfcbbde7355fe0aa71e162f82190d7bcc
Jonathan Berhe <jonathan@tate.energy>
2018-11-24 22:17:57 +0100

    add games

---
+++ index.js
@@ -0,0 +1,127 @@
+class Vector {
+    constructor(x = 0, y = 0) {
+        this.x = x
+        this.y = y
+    }
+}
+
+class Rectangle {
+    constructor(w, h) {
+        this.position = new Vector
+        this.size = new Vector(w, h)
+    }
+    get left() {
+        return this.position.x - this.size.x / 2
+    }
+    get right() {
+        return this.position.x + this.size.x / 2
+    }
+    get top() {
+        return this.position.y - this.size.y / 2
+    }
+    get bottom() {
+        return this.position.y + this.size.y / 2
+    }
+}
+
+class Ball extends Rectangle {
+    constructor() {
+        super(10, 10)
+        this.vel = new Vector
+    }
+
+    setDefaultVelocity(velocity) {
+        this.vel.x = velocity
+        this.vel.y = velocity
+    }
+    setDefaultPosition(x = 132, y = 190) {
+        this.position.x = x
+        this.position.y = y
+    }
+}
+
+class Player extends Rectangle {
+    constructor() {
+        super(20, 100)
+        this.score = 0
+    }
+}
+
+class Pong {
+    constructor(canvas) {
+        this._canvas = canvas
+        this._context = canvas.getContext('2d')
+
+        this.players = [new Player, new Player]
+        this.players[0].position.x = 40
+        this.players[1].position.x = this._canvas.width - 40
+        for (const player of this.players) player.position.y = this._canvas.height / 2
+
+        this.ball = new Ball
+        this.ball.setDefaultVelocity(600)
+        this.ball.setDefaultPosition()
+
+        this.updateAnimationFrame();
+    }
+
+    updateAnimationFrame() {
+        let lastTime = null
+        const callback = (millis) => {
+            if (lastTime)
+                this.updatePosition((millis - lastTime) / 1000);
+            lastTime = millis;
+            requestAnimationFrame(callback);
+        };
+        callback();
+    }
+
+    collide(player, ball){
+        if( player.left < ball.right && player.right > ball.left && 
+            player.top < ball.bottom && player.bottom > ball.top ){
+                ball.vel.x = -ball.vel.x
+            }
+    }
+
+    drawRectangleElement(rect) {
+        this._context.fillStyle = '#fff'
+        this._context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y)
+        //console.log('ball: ', ...rect)
+    }
+
+    drawAll() {
+        // Draw background
+        this._context.fillStyle = '#000'
+        this._context.fillRect(0, 0, this._canvas.width, this._canvas.height)
+
+        this.drawRectangleElement(this.ball)
+
+        for (const player of this.players) this.drawRectangleElement(player)
+    }
+
+    updatePosition(dt) {
+        this.ball.position.x += this.ball.vel.x * dt
+        this.ball.position.y += this.ball.vel.y * dt
+
+        if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
+            this.ball.vel.x = -this.ball.vel.x
+        }
+
+        if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+            this.ball.vel.y = -this.ball.vel.y
+        }
+        // user player
+        this.players[1].position.y = this.ball.position.y
+        for(const player of this.players) this.collide(player, this.ball)
+
+        this.drawAll()
+    }
+}
+
+const canvas = document.getElementById('pong')
+
+const pong = new Pong(canvas)
+
+// CPU player
+canvas.addEventListener('mousemove', event => {
+    pong.players[0].position.y = event.offsetY
+})
